2bffa34218b1de4c47bed3a37009c2f0e561e216
Malo Le Magueresse <36453049+malo-le-magueresse@users.noreply.github.com>
2023-07-18 16:47:21 -0700

    my_constants.py files

---
+++ imessage/my_constants.py
@@ -0,0 +1,7 @@
+### DATA URL
+URL = "weather_data_url"
+
+#### CONSTANTS
+DB_PATH = '~/Library/Messages/chat.db' # The file where iMessages are stored
+RECIPIENT_NUMBERS = ['phone_numbers'] # The numbers we want to automate the sending of messages to
+SCRIPT_PATH = "path to the AppleScript file" # The path to the AppleScript file that actually sends the message
---
+++ imessage_gpt/my_constants.py
@@ -0,0 +1,10 @@
+OPEN_AI_KEY = "your_open_ai_key"
+OPEN_AI_ORG_ID = "your_open_ai_org_id"
+OPEN_AI_MODEL_ID = "your_open_ai_model_id"
+DB_PATH = '~/Library/Messages/chat.db'
+ALLOWED_RECIPIENT_NUMBERS = {
+    'phone_number': 'contact name',
+    'phone number': 'contact name',
+}
+SCRIPT_PATH = "path to the AppleScript file"
+YOUR_NAME = "your name"
